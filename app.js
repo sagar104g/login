290c675575f8d4f7a8569dd957262681e3ea9ddd
sagar104gurgaon <sagar104gurgaon@gmail.com>
2019-07-21 00:38:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const login = require('./routes/login');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/login',login);
+
+app.listen(3000,function(err){
+    console.log("listining on port 3k")
+})
